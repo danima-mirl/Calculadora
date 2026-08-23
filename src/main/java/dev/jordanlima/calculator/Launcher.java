dc1da131a203e608454914d9514d07c54989d9bf
Jordan Lima <jordanlimasuhett@gmail.com>
2026-08-23 01:22:40 -0300

    Alteração no layout e interações nos botões

---
+++ src/main/java/dev/jordanlima/calculator/Launcher.java
@@ -0,0 +1,9 @@
+package dev.jordanlima.calculator;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
